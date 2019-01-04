103329d2b0a9c3246d765bdaf3baa5378b57c775
xueliang <1058392650@qq.com>
2019-01-04 14:17:55 +0800

    chart

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
     <div id="app">
         <span>123</span>
         <!-- <img src="static/image/163-1.png" alt="ai" width="100"> -->
-        <!-- <ChartVue :width='600' :height='400'></ChartVue> -->
         <img src="@/asset/123.jpg" alt="" width="100">
     </div>
 </template>
 
 <script>
-import ChartVue from '@/components/ChartVue'
+
 export default {
     name: "App",
-    components:{
-        ChartVue
-    },
     created(){
         
     }
