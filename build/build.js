86a7e987f935e7137bb08e5ad7285cd3438287fc
xueliang <xueliang@szusoft.com>
2019-02-21 16:50:14 +0800

    ora

--- build/build.js
+++ build/build.js
@@ -1,14 +1,18 @@
 const webpack = require('webpack')
 const config = require('./webpack.pro.config')
+const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
 
+const spinner = ora('build start...')
+spinner.start();
 // 打包前删除出口文件夹
 rm(path.join(path.resolve(__dirname, '../dist'), 'static'), err => {
     if (err) {
         throw new Error(err)
     }
     webpack(config, (err, stats) => { // 打包函数
+        spinner.stop();
         process.stdout.write(stats.toString({
             colors: true,
             modules: false,
