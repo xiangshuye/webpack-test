49dbe993531449012c5c0eda4debeb3a941da2ae
xueliang <1058392650@qq.com>
2019-10-17 11:01:15 +0800

    Update permission.js

--- src/router/permission.js
+++ src/router/permission.js
@@ -3,20 +3,25 @@ import router from './index';
 // 根据业务逻辑 待完善
 
 router.beforeEach((to, from, next) => {
-    console.log(to);
-    let user = sessionStorage.getItem("username");
-    if (user == null && to.path != '/login') {
-      sessionStorage.clear();
-      next('/login');
-    } else {
-      if (to.path == '/login') {
+    let user = sessionStorage.getItem('sportHealthUserName');
+    if (!user && to.path !== '/login') {
         sessionStorage.clear();
-        next();
-      } else if(to.path === '/'){
-        next('/index');
-      }else{
-        next();
-      }
+        next('/login');
+    } else {
+        if (to.path === '/login' || to.path === '/') {
+            sessionStorage.clear();
+            next();
+        } else if (to.path === '/index') {
+            next();
+        } else {
+            let permission = JSON.parse(sessionStorage.getItem('permission'));
+            permission = permission || [];
+            if (permission.indexOf(to.name) !== -1) {
+                next();
+            } else {
+                next('/index');
+            }
+        }
     }
 })
 
