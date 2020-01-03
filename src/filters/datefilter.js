57c451fc2e6051fc545311db61fe15f598d917bb
xueliang <1058392650@qq.com>
2020-01-03 14:50:40 +0800

    filter

---
+++ src/filters/datefilter.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import { dateFormatFromString, datetimeformatFromString} from '@/utils/dateUtils'
+
+Vue.filter('numbertoDate', function (value) {
+    if (!value) {
+        return '';
+    }
+    return dateFormatFromString(value);
+});
+
+Vue.filter('timestampToDatetime', function (timestamp) {
+    if (!value) {
+        return '';
+    }
+    return datetimeformatFromString(value);
+});
