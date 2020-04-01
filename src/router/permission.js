8da6c32cfce84f598964a2cde085e7821ea1420d
xueliang <1058392650@qq.com>
2020-04-01 16:44:52 +0800

    Update permission.js

--- src/router/permission.js
+++ src/router/permission.js
@@ -1,5 +1,6 @@
 import router from './index';
 import store from '@/store/index';
+import Vue from 'vue';
 // 根据业务逻辑 待完善
 
 router.beforeEach((to, from, next) => {
@@ -16,7 +17,10 @@ router.beforeEach((to, from, next) => {
         } else {
             store.dispatch('getRouter').then(() => {
                 router.addRoutes(store.state.permission.addRouter);
-                next({...to}) // hack方法 确保addRoutes已完成
+                Vue.nextTick(() => {
+                    next({ ...to }) // hack方法 确保addRoutes已完成
+                })
+
             })
             // let permission = JSON.parse(sessionStorage.getItem('permission'));
             // permission = permission || [];
