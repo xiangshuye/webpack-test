16f88dadb3ea3ebb9e0aca6ae8538ea7735760c3
xueliang <1058392650@qq.com>
2020-04-01 13:31:28 +0800

    leyout

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="layout">
     <Layout>
-      <Header>
-        <MyHeader></MyHeader>
-      </Header>
-      <Layout>
-        <Sider hide-trigger  :style="{width: '234px', minWidth: '234px', maxWidth: '234px'}">
-          <MySide></MySide>
-        </Sider>
-        <Layout :style="{padding: '0 10px'}">
-          <Content :style="{padding: '10px', background: '#fff'}">
-            <router-view></router-view>
-          </Content>
+        <Header>
+            <MyHeader></MyHeader>
+        </Header>
+        <Layout>
+            <Sider hide-trigger :width="234">
+                <MySide></MySide>
+            </Sider>
+            <Layout :style="{ padding: '0 10px' }">
+                <Content :style="{ padding: '10px', background: '#fff' }">
+                    <router-view></router-view>
+                </Content>
+            </Layout>
         </Layout>
-      </Layout>
     </Layout>
-  </div>
 </template>
 
 <script>
@@ -23,12 +21,11 @@ import MyHeader from "./MyHeader";
 import MySide from "./MySide";
 
 export default {
-  name: "MyLayout",
-  components: {
-    MyHeader,
-    MySide
-  }
+    name: "MyLayout",
+    components: {
+        MyHeader,
+        MySide
+    }
 };
 </script>
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
