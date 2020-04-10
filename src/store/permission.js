266540bd0b6cb5e5cd764c0e09060ed5f9e50435
xueliang <1058392650@qq.com>
2020-04-10 10:55:53 +0800

    pe

--- src/store/permission.js
+++ src/store/permission.js
@@ -34,7 +34,7 @@ let permission = {
         routers: routers,
         addRouter: [],
         permissions: ["account", "accountInfo", "notice", "noticeInfo", "message", "messageStatistics", "templateCheck", "signatureCheck", "user", "department", "userInfo", "roleInfo", "monitorCenter", "monitorMsg", "conLog", "operationlog", "one", "two", "three1", "three2", "three3", "three4", "three5", "two2", "three6", "three7", "three8", "three9", "two3", "three10", "three11", "two4", "three12", "two5"],
-        perm: ["5db7d51a1c9f0fc9f4fd7400", "5db7d5431c9f0fc9f4fd740e", "5db7d5751c9f0fc9f4fd7428", "5db7d5b61c9f0fc9f4fd7449", "5db7d5d71c9f0fc9f4fd745c", "5db7d5e41c9f0fc9f4fd7462", "5db7d5f21c9f0fc9f4fd7469", "5db7d6011c9f0fc9f4fd7470", "5e8fc1ec161e5a39dab79c8d", "5e8fc1f8161e5a39dab79c96", "5e8fc205161e5a39dab79c9c", "5e8fc231161e5a39dab79cb5", "5e8fc239161e5a39dab79cba", "5e8fc254161e5a39dab79ccc"],
+        perm: ["5db7d4e41c9f0fc9f4fd73e3", "5db7d50b1c9f0fc9f4fd73f7", "5db7d5751c9f0fc9f4fd7428", "5db7d5b61c9f0fc9f4fd7449", "5db7d5d71c9f0fc9f4fd745c", "5db7d5e41c9f0fc9f4fd7462", "5db7d5f21c9f0fc9f4fd7469", "5db7d6011c9f0fc9f4fd7470", "5e8fc1ec161e5a39dab79c8d", "5e8fc1f8161e5a39dab79c96", "5e8fc205161e5a39dab79c9c", "5e8fc231161e5a39dab79cb5", "5e8fc239161e5a39dab79cba", "5e8fc254161e5a39dab79ccc"],
         refresh: false
     },
     mutations: {
