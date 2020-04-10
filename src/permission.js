00e9f320f6074da25ada95e7a7729ba613d39243
xueliang <1058392650@qq.com>
2020-04-10 10:50:46 +0800

    路由守卫

--- src/permission.js
+++ src/permission.js
@@ -1,32 +1,31 @@
 import router from './router/index';
 import store from '@/store/index';
-// import Vue from 'vue';
-// 根据业务逻辑 待完善
 
+// 根据业务逻辑 待完善
 router.beforeEach((to, from, next) => {
-    // let user = store.state.user.username;
-    if (!sessionStorage.getItem('username') && to.path !== '/login') {
+    if (to.path === '/login') {
         sessionStorage.clear();
-        next('/login');
-    } else {
-        if (to.path === '/login') {
-            sessionStorage.clear();
-            next();
-        } else if (store.state.permission.refresh) {
+        next();
+    } else if (sessionStorage.getItem('username')) {
+        if (store.state.permission.refresh) {
             next();
         } else {
             store.dispatch('getRouter').then(() => {
                 router.addRoutes(store.state.permission.addRouter);
                 next({ ...to }) // hack方法 确保addRoutes已完成
             })
-            // let permission = JSON.parse(sessionStorage.getItem('permission'));
-            // permission = permission || [];
-            // if (permission.includes(to.name)) {
-            //     next();
-            // } else {
-            //     next('/index');
-            // }
         }
+    } else {
+        next('/login');
     }
+
+    // let permission = JSON.parse(sessionStorage.getItem('permission'));
+    // permission = permission || [];
+    // if (permission.includes(to.name)) {
+    //     next();
+    // } else {
+    //     next('/index');
+    // }
+
 })
 
