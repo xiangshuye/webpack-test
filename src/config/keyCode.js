4b937862983c11b732cfbcbdebcaa69abfb368ab
xueliang <1058392650@qq.com>
2020-05-19 09:52:12 +0800

    code map

---
+++ src/config/keyCode.js
@@ -0,0 +1,4 @@
+export const BMAMKEY = 'eIGwkbkGLzFGy3bVduSUXlBcPMQEM5fi';
+export const AMAPKEY = '425d4ea45f9e4df0b51323634035c434';
+export const SUCCESS = 200;
+export const LOGIN_TIMEOUT = 300;
