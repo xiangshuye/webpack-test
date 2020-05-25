b8cdda3be7122d4ffd7363c77a55c35faa982c67
xueliang <xueliang@szusoft.com>
2020-05-25 13:51:36 +0800

    fetch

--- src/permission.js
+++ src/permission.js
@@ -1,31 +1,25 @@
-import router from './router/index';
-import store from '@/store/index';
+import router from "./router/index";
+import store from "@/store/index";
 
 // 根据业务逻辑 待完善
 router.beforeEach((to, from, next) => {
-    if (to.path === '/login') {
+    if (to.path === "/login") {
         sessionStorage.clear();
         next();
-    } else if (sessionStorage.getItem('username')) {
+    } else if (sessionStorage.getItem("username")) {
         if (store.state.permission.refresh) {
             next();
         } else {
-            store.dispatch('getRouter').then(() => {
+            store.dispatch("getRouter").then(() => {
                 router.addRoutes(store.state.permission.addRouter);
-                next({ ...to }) // hack方法 确保addRoutes已完成
-            })
+                next({ ...to }); // hack方法 确保addRoutes已完成
+            });
         }
     } else {
-        next('/login');
+        next("/login");
     }
+});
 
-    // let permission = JSON.parse(sessionStorage.getItem('permission'));
-    // permission = permission || [];
-    // if (permission.includes(to.name)) {
-    //     next();
-    // } else {
-    //     next('/index');
-    // }
-
-})
-
+router.afterEach((to, from) => {
+    document.title = to.meta.title;
+});
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,35 +1,34 @@
-import axios from 'axios';
-import { SUCCESS, LOGIN_TIMEOUT} from '@/config/keyCode'
+import axios from "axios";
+import { SUCCESS, LOGIN_TIMEOUT } from "@/config/keyCode";
 
 /*
  * 对 axios 进行封装，可以利用拦截器功能对发送数据和返回数据进行处理，或其他操作
  * 比如：使用 token 进行身份验证
-*/
+ */
 
 let fetch = axios.create({
-    baseURL: 'http://localhost:3002/api/',
+    baseURL: "http://localhost:3002/api/",
     timeout: 10000,
-    withCredentials: true,
-})
+    withCredentials: true
+});
 fetch.interceptors.request.use(config => {
-    const token = sessionStorage.getItem('token');
+    const token = sessionStorage.getItem("token");
     if (token) {
-        config.headers['token'] = token;
+        config.headers["token"] = token;
     }
-    return token;
-})
+    return config;
+});
 fetch.interceptors.response.use(res => {
     if (res.status === 200) {
         const data = res.data;
         const operCode = data.code;
-        if (operCode === SUCCESS) { // 请求成功
-
-        } else if (operCode === LOGIN_TIMEOUT) { // 登录超时，token过期，返回 login 页面
-
+        if (operCode === SUCCESS) {
+            // 请求成功
+        } else if (operCode === LOGIN_TIMEOUT) {
+            // 登录超时，token过期，返回 login 页面
         }
         return data;
     }
-})
-
+});
 
 export default fetch;
