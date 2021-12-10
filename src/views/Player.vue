b2bad5b972adc7db7ef5164fc988df134110ae82
xueliang <xueliang@successhr.com>
2021-12-10 09:01:14 +0800

    Update Player.vue

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -2,8 +2,8 @@
   <div id="player"></div>
 </template>
 <script>
-import Player from "xgplayer/dist/simple_player";
-import { volume, playbackRate } from "xgplayer/dist/controls";
+// import Player from "xgplayer/dist/simple_player";
+// import { volume, playbackRate } from "xgplayer/src/controls";
 
 export default {
   name: "Player",
@@ -12,20 +12,20 @@ export default {
   },
   methods: {
     init() {
-      let player = new Player({
-        id: "player",
-        width: "100%",
-        height: "500px",
-        url: "http://localhost:9101/public/images/123.mp4",
-        controlPlugins: [volume, playbackRate],
-        playbackRate: [0.5, 0.75, 1, 1.5, 2] //传入倍速可选数组
-      });
-      console.log(player);
-    }
+      //    let player = new Player({
+      //     id: "player",
+      //     width: '100%',
+      //     height: '500px',
+      //     url: "http://localhost:9101/public/images/123.mp4",
+      //     controlPlugins: [volume, playbackRate],
+      //     playbackRate: [0.5, 0.75, 1, 1.5, 2], //传入倍速可选数组
+      //    });
+      //    console.log(player);
+    },
+    mounted() {
+      this.init();
+    },
   },
-  mounted() {
-    this.init();
-  }
 };
 </script>
 <style lang="less">
