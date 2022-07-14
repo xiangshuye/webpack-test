dbffdba82bfef678bd8633fe39df3da352afbeb5
xueliang <1058392650@qq.com>
2022-07-14 18:03:00 +0800

    corejs

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 export default {
   name: "App",
   mounted() {
+    console.log(Promise.allSettled)
     Promise.allSettled([]).then(() => {});
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'core-js'
 import Vue from "vue";
 import router from "@/router/index";
 import store from "@/store/index";
