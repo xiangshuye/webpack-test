358c996b3914f891a7201dff3d92e8c2417bae04
xueliang <1058392650@qq.com>
2022-11-17 21:20:26 +0800

    key

--- src/config/keyCode.js
+++ src/config/keyCode.js
@@ -1,5 +1,5 @@
-export const BMAMKEY = "eIGwkbkGLzFGy3bVduSUXlBcPMQEM5fi";
-export const AMAPKEY = "425d4ea45f9e4df0b51323634035c434";
+export const BMAMKEY = "7wPvKCwKGGNvZlRBVHCnAk7G4bIpZwWG";
+export const AMAPKEY = "a100d201cb10b1ca291a85ac6b9c1f62";
 export const SUCCESS = 200;
 export const LOGIN_TIMEOUT = 300;
 export const RESOURCEBASEPATH = "/"; //资源基本路径
